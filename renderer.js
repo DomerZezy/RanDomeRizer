97eb79e367eec618fc7ebb72e9739ca056ee2550
DomerZezy <sdomeracki1@gmail.com>
2022-03-02 16:17:22 +0100

    v1.0.0 commit #2 - see last commit for changes VSCode GitHub integration had a stroke and didn't add my files to commit

--- renderer.js
+++ renderer.js
@@ -1,6 +1,5 @@
-const { remove, write } = require("fs-jetpack");
-
-console.log("renderer loaded");
+const { remove, write, exists, read } = require("fs-jetpack");
+const { remote } = require("electron");
 
 const addFieldButton = document.querySelector(".main__addFieldButton");
 const addGroupButton = document.querySelector(".main__addGroupButton");
@@ -8,23 +7,83 @@ const groupsList = document.querySelector(".main__groupsList");
 const fieldsList = document.querySelector(".main__fieldsList");
 const randomizeButton = document.querySelector(".main__randomizeButton");
 const results = document.querySelector(".main__results");
+const blur = document.querySelector(".main__blur");
+const loadLastSetupButton = document.querySelector(
+  ".main__loadLastSetupButton"
+);
+const loadLastResultsButton = document.querySelector(
+  ".main__loadLastResultButton"
+);
+
+let firstGroup = false;
+let firstField = false;
+
+const renderResults = (resultsList, groups) => {
+  results.style.left = "10%";
+  blur.style.left = "0";
+  groups.forEach((group) => {
+    results.innerHTML += `<h2 class="main__resultsTitle" style="color: ${group.groupColor};">${group.groupName}</h2>`;
+    resultsList.forEach((result) => {
+      if (result.group.groupName === group.groupName)
+        results.innerHTML += `<p class="main__resultsResult">${result.field}</p>`;
+    });
+  });
+};
+
+loadLastResultsButton.addEventListener("click", () => {
+  if (exists("lastRandomize.json")) {
+    const data = read("lastRandomize.json", "json");
+    const groups = [];
+
+    data.forEach((piece) => {
+      if (!groups.some((x) => x.groupName === piece.group.groupName)) {
+        groups.push({
+          groupName: piece.group.groupName,
+          groupColor: piece.group.groupColor,
+        });
+      }
+    });
+    renderResults(data, groups);
+  }
+});
 
 addGroupButton.addEventListener("click", () => {
+  if (!firstGroup) {
+    groupsList.innerHTML = `<p class="main__groupsListTitle">Groups</p>`;
+    firstGroup = true;
+  }
   groupsList.innerHTML += `
   <div class="main__group">
     <input class="main__groupName" type="text" placeholder="Group name">
-    <input class="main__groupColor" type="text" placeholder="Group color(hex) ex. #FF0000 for red">
+    <input class="main__groupColor" type="text" placeholder="Group color(hex)">
     <button class="main__removeGroupButton">Remove group</button>
   </div>
   `;
+  document.querySelectorAll(".main__groupColor").forEach((color) => {
+    color.addEventListener("input", (e) => {
+      const data = e.target.value;
+      if (data.length === 4 || (data.length === 7 && data.charAt(0) === "#")) {
+        e.target.style.border = `1px solid ${data}`;
+      }
+    });
+  });
+
   document.querySelectorAll(".main__removeGroupButton").forEach((button) => {
     button.addEventListener("click", (e) => {
+      if (document.querySelectorAll(".main__group").length === 1) {
+        firstGroup = false;
+        document.querySelector(".main__groupsListTitle").remove();
+      }
       e.target.parentNode.remove();
     });
   });
 });
 
 addFieldButton.addEventListener("click", () => {
+  if (!firstField) {
+    fieldsList.innerHTML = `<p class="main__fieldsListTitle">Fields</p>`;
+    firstField = true;
+  }
   fieldsList.innerHTML += `
   <div class="main__field">
     <input class="main__fieldName" type="text" placeholder="Field name">
@@ -33,6 +92,64 @@ addFieldButton.addEventListener("click", () => {
   `;
   document.querySelectorAll(".main__removeFieldButton").forEach((button) => {
     button.addEventListener("click", (e) => {
+      if (document.querySelectorAll(".main__field").length === 1) {
+        firstField = false;
+        document.querySelector(".main__fieldsListTitle").remove();
+      }
+      e.target.parentNode.remove();
+    });
+  });
+});
+
+loadLastSetupButton.addEventListener("click", () => {
+  fieldsList.innerHTML = `<p class="main__fieldsListTitle">Fields</p>`;
+  groupsList.innerHTML = `<p class="main__groupsListTitle">Groups</p>`;
+
+  if (exists("lastSetup.json")) {
+    const lastSetup = read("lastSetup.json", "json");
+    lastSetup.fields.forEach((field) => {
+      firstField = true;
+      fieldsList.innerHTML += `
+        <div class="main__field">
+          <input class="main__fieldName" type="text" placeholder="Field name" value="${field}">
+          <button class="main__removeFieldButton">Remove field</button>
+        </div>
+      `;
+    });
+    lastSetup.groups.forEach((group) => {
+      firstGroup = true;
+      groupsList.innerHTML += `
+        <div class="main__group">
+          <input class="main__groupName" type="text" placeholder="Group name" value="${group.groupName}">
+          <input class="main__groupColor" type="text" style="border: 1px solid ${group.groupColor}" placeholder="Group color(hex)" value="${group.groupColor}">
+          <button class="main__removeGroupButton">Remove group</button>
+        </div>
+        `;
+    });
+  }
+  document.querySelectorAll(".main__groupColor").forEach((color) => {
+    color.addEventListener("input", (e) => {
+      const data = e.target.value;
+      if (data.length === 4 || (data.length === 7 && data.charAt(0) === "#")) {
+        e.target.style.border = `1px solid ${data}`;
+      }
+    });
+  });
+  document.querySelectorAll(".main__removeFieldButton").forEach((button) => {
+    button.addEventListener("click", (e) => {
+      if (document.querySelectorAll(".main__field").length === 1) {
+        firstField = false;
+        document.querySelector(".main__fieldsListTitle").remove();
+      }
+      e.target.parentNode.remove();
+    });
+  });
+  document.querySelectorAll(".main__removeGroupButton").forEach((button) => {
+    button.addEventListener("click", (e) => {
+      if (document.querySelectorAll(".main__group").length === 1) {
+        firstGroup = false;
+        document.querySelector(".main__groupsListTitle").remove();
+      }
       e.target.parentNode.remove();
     });
   });
@@ -63,27 +180,36 @@ randomizeButton.addEventListener("click", () => {
     groups: groups,
   };
 
-  console.log(fields);
-  console.log(groups);
-  console.log(data);
-
   remove("lastSetup.json");
 
   write("lastSetup.json", data);
 
   const usedNumbers = [];
   const resultsList = [];
-
-  for (let i = 0; i < fields.length; i++) {
-    let done = false;
-    for (let j = 0; j < groups.length; j++) {
+  for (let i = 0; i < groups.length; i++) {
+    for (let j = 0; j < Math.ceil(fields.length / groups.length); j++) {
+      let done = false;
       while (!done) {
         const randomNumber = Math.floor(Math.random() * fields.length);
-        if (!usedNumbers.some(randomNumber)) {
+        if (usedNumbers.length === fields.length) {
+          done = true;
+          continue;
+        }
+        if (!usedNumbers.some((x) => x === randomNumber)) {
           usedNumbers.push(randomNumber);
-          const result = "";
+          const result = {
+            group: groups[i],
+            field: fields[randomNumber],
+          };
+          resultsList.push(result);
+          done = true;
         }
       }
     }
   }
+
+  remove("lastRandomize.json");
+  write("lastRandomize.json", resultsList);
+
+  renderResults(resultsList, groups);
 });
